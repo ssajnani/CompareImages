a4736664f66f875d1b849ff4020bb6da64353be1
Samar Sajnani <ssajnani@uwo.ca>
2019-07-08 11:18:52 -0400

    Fix main.py /bin/python bug, add better comments, update setup.sh

--- analysis/feature_extractor.py
+++ analysis/feature_extractor.py
@@ -5,15 +5,17 @@ from imageio import imread
 import _pickle as cPickle
 import random
 import os
-import matplotlib.pyplot as plt
 import pickle
 
-''' Feature extractor
-@param image_path string
-@param vector_size int
-@return numpy matrix of features
-'''
 def extract_features(image_path, vector_size=32):
+    
+    ''' Feature extractor
+    Extracts the features of an image at the image path provided.
+    The features are extracted using the SURF computer vision algorithm
+    @param image_path string
+    @param vector_size int
+    @return numpy matrix of features
+    '''
     try:
         image = imread(image_path, pilmode="RGB")
         # Using SURF because it has been demostrated as it is among the best feature detectors: 
